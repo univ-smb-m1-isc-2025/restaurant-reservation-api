f91b2768b737241e1bb1c59d5037be19a4b0d1bb
nico-qrnta <nicolas291101@gmail.com>
2025-02-07 14:44:14 +0100

    first commit

---
+++ src/main/java/m1/info/reza/RezaApplication.java
@@ -0,0 +1,13 @@
+package m1.info.reza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/m1/info/reza/RezaApplicationTests.java
@@ -0,0 +1,13 @@
+package m1.info.reza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
