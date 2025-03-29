ba1cc2d31d9457ed356092fc2a7976ec9c8fca3f
nico-qrnta <nicolas291101@gmail.com>
2025-03-29 18:17:32 +0100

    :rotating_light: - fix linter warning

--- src/main/java/m1/info/reza/configs/WebConfig.java
+++ src/main/java/m1/info/reza/configs/WebConfig.java
@@ -1,6 +1,5 @@
 package m1.info.reza.configs;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
