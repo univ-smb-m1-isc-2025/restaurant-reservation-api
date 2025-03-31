61fb111aaa583dfdd246c5b407b0e439b79b42aa
nico-qrnta <nicolas291101@gmail.com>
2025-03-31 15:08:01 +0200

    :art: - fix

--- src/main/java/m1/info/reza/planning/opening/RestaurantOpeningRepository.java
+++ src/main/java/m1/info/reza/planning/opening/RestaurantOpeningRepository.java
@@ -1,6 +1,6 @@
 package m1.info.reza.planning.opening;
 
-import m1.info.reza.planning.RestaurantOpening;
+import m1.info.reza.planning.opening.RestaurantOpening;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantOpeningRepository extends JpaRepository<RestaurantOpening, Long> {
