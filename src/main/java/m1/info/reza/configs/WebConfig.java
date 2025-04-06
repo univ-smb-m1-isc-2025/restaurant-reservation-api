9372ce818b03b230ba81c6dd2b0732d08b72bbe3
nico-qrnta <nicolas291101@gmail.com>
2025-04-06 21:33:20 +0200

    :bug:  - update cors config

--- src/main/java/m1/info/reza/configs/WebConfig.java
+++ src/main/java/m1/info/reza/configs/WebConfig.java
@@ -16,13 +16,9 @@ public class WebConfig implements WebMvcConfigurer {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.addAllowedOrigin("http://localhost:4200");
         corsConfig.addAllowedOrigin("http://www.reza.oups.net");
-        corsConfig.addAllowedMethod("GET");
-        corsConfig.addAllowedMethod("POST");
-        corsConfig.addAllowedMethod("PUT");
-        corsConfig.addAllowedMethod("DELETE");
-        corsConfig.addAllowedMethod("OPTIONS");
-        corsConfig.addAllowedHeader("*"); // Autoriser tous les en-têtes
-        corsConfig.setAllowCredentials(true); // Si vous devez autoriser les cookies ou l'authentification
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
 
         // Appliquer la configuration à toutes les URLs
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
